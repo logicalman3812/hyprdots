7e8ccd90c3a2ad9cf25652de4407ff87eaee99c5
logicalman3812 <artemstres@gmail.com>
2024-10-08 22:41:34 +0500

    chore: removed swww path

--- .config/wal/colorschemes/dark/catppuccin/nvim/lua/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local function set_colorscheme(colorscheme)
-  local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-  if not status_ok then
-    vim.notify("Colorscheme " .. colorscheme .. " not found!", vim.log.levels.ERROR)
-  end
-end
-
-set_colorscheme("catppuccin-mocha")
