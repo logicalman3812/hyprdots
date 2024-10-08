3ea11100d89a8c84cc7009d1e27734775bb3341a
logicalman3812 <artemstres@gmail.com>
2024-10-08 22:33:39 +0500

    mirgated from kitty

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
+-- config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 16.0
 
 config.enable_tab_bar = false
@@ -13,5 +14,8 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
 config.enable_scroll_bar = false
+config.default_cursor_style = 'SteadyBlock'
+config.force_reverse_video_cursor = true
+config.window_close_confirmation = 'NeverPrompt'
 
 return config
